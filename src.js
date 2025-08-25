d19f25940619f480b91b62a928b4bbbc7214bbe9
xiaoye <caob@xiaoyetech.team>
2025-08-25 18:26:39 +0800

    1

--- src.js
+++ src.js
@@ -72,7 +72,8 @@ export default {
         const 配置生成器 = {
           v2ray: v2ray配置文件,
           clash: clash配置文件,
-          tips: 提示界面,
+          tips: v2ray配置文件,
+          //tips: 提示界面,
         };
         const 工具 = Object.keys(配置生成器).find((工具) => 用户代理.includes(工具));
         优选列表 = await 获取优选列表();
