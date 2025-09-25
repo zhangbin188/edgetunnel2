23cfb4e63da6aee4b8021bfa63789e5700dbf7c6
1 <1@1.com>
2025-09-25 21:17:00 +0800

    1

--- _worker.js
+++ _worker.js
@@ -87,7 +87,7 @@ export default {
         const 配置生成器 = {
           [威图锐]: 威图锐配置文件,
           [科拉什]: 科拉什配置文件,
-          "tips": 提示界面,
+          tips: 提示界面,
         };
         const 工具 = Object.keys(配置生成器).find((工具) => 用户代理.includes(工具));
         优选列表 = await 获取优选列表();
